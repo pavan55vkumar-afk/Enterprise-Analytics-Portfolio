aed5856928438d20c51d226344c187d39966eede
pavan kumar vaikutherisal <pavan55vkumar@gmail.com>
2026-07-31 13:01:01 +0530

    Completely remove project_2_operations_rca directory from repository

--- project_2_operations_rca/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Forwarding stub for Streamlit Cloud entry point
-root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if root_dir not in sys.path:
-    sys.path.insert(0, root_dir)
-
-import project_1_operations_rca.app
